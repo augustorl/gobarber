5ee243e00cb336ea7bd1a5a184a3ffe01af2357f
augustorl <gutin22@gmail.com>
2020-06-24 19:52:49 -0300

    creating mobile test

---
+++ Mobile/src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-native-testing-library';
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+describe('SignIn Page', () => {
+  it('should contain email/password inputs', () => {
+    const { getByPlaceholder } = render(<SignIn />);
+
+    expect(getByPlaceholder('E-mail')).toBeTruthy();
+    expect(getByPlaceholder('Senha')).toBeTruthy();
+  });
+});
